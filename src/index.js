8b8b0b09877abb32db295b9a7dd078277feca480
red-runner <pol@lodgify.com>
2019-04-08 16:32:15 +0200

    fix(index): update file paths

--- src/index.js
+++ src/index.js
@@ -1,23 +1,22 @@
 export {
-    getAvailability,
-    AVAILABILITY_NOT_AVAILABLE,
-    AVAILABILITY_CHECK_IN,
-    AVAILABILITY_CHECK_OUT,
-  } from './resources';
-  export { postCallMeBack } from './resources';
-  export { postContact } from './resources';
-  export { getCurrencies } from './resources';
-  export { postModel } from './resources';
-  export { getImage } from './resources';
-  export { getLocalization } from './resources';
-  export { getPayments } from './resources';
-  export { getPromotion } from './resources';
-  export { getRates } from './resources';
-  export { postRecoverPassword } from './resources';
-  export { getReviews, postReview } from './resources';
-  export { getRoomType } from './resources';
-  export { postSignup } from './resources';
-  export { getTimeOptions } from './resources';
-  export { getTranslations } from './resources';
-  export { LOGIN_REDIRECT_URL } from './constants';
-  
+  getAvailability,
+  AVAILABILITY_NOT_AVAILABLE,
+  AVAILABILITY_CHECK_IN,
+  AVAILABILITY_CHECK_OUT,
+} from './websites-service-client/index';
+export { postCallMeBack } from './websites-service-client/index';
+export { postContact } from './websites-service-client/index';
+export { getCurrencies } from './websites-service-client/index';
+export { postModel } from './websites-service-client/index';
+export { getImage } from './websites-service-client/index';
+export { getLocalization } from './websites-service-client/index';
+export { getPayments } from './websites-service-client/index';
+export { getPromotion } from './websites-service-client/index';
+export { getRates } from './websites-service-client/index';
+export { postRecoverPassword } from './websites-service-client/index';
+export { getReviews, postReview } from './websites-service-client/index';
+export { getRoomType } from './websites-service-client/index';
+export { postSignup } from './websites-service-client/index';
+export { getTimeOptions } from './websites-service-client/index';
+export { getTranslations } from './websites-service-client/index';
+export { LOGIN_REDIRECT_URL } from './websites-service-client/index';
