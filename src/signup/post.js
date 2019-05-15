6a1fd0e4859ab3305692453b6c7b09aa6455032c
Jonny Gwillim <jonnygwillim@gmail.com>
2019-05-15 12:30:15 +0200

    feat(signup): accept recaptcha token

--- src/signup/post.js
+++ src/signup/post.js
@@ -4,7 +4,8 @@ import { PATHNAME_TEMPLATE } from './constants';
 /**
  * @param  {number} websiteId
  * @param  {Object} formValues
+ * @param  {string} reCaptchaToken
  * @return {Promise}
  */
-export const post = (websiteId, formValues) =>
-  postForm(PATHNAME_TEMPLATE, websiteId, formValues);
+export const post = (websiteId, formValues, reCaptchaToken) =>
+  postForm(PATHNAME_TEMPLATE, websiteId, formValues, reCaptchaToken);
