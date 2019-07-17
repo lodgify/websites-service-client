d668c26897566eff19c5636de897cdbdfe33c5ca
Jonny Gwillim <jonnygwillim@gmail.com>
2019-07-17 11:02:49 +0200

    fix(availability): expose constants

--- src/availability/index.js
+++ src/availability/index.js
@@ -2,5 +2,6 @@ export { get as getAvailability } from './get';
 export {
   AVAILABILITY_NOT_AVAILABLE,
   AVAILABILITY_CHECK_IN,
+  AVAILABILITY_CHECK_IN_AND_OUT,
   AVAILABILITY_CHECK_OUT,
 } from './constants';
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ export {
   getAvailability,
   AVAILABILITY_NOT_AVAILABLE,
   AVAILABILITY_CHECK_IN,
+  AVAILABILITY_CHECK_IN_AND_OUT,
   AVAILABILITY_CHECK_OUT,
 } from './availability';
 export { postCallMeBack } from './callmeback';
