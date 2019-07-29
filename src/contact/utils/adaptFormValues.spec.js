d9f0bb2e7b8a2957469c2cee7fc45d42e42faa48
Jonny Gwillim <jonnygwillim@gmail.com>
2019-07-29 17:29:22 +0200

    fix(contact): adapt propertyId and dates

--- src/contact/utils/adaptFormValues.spec.js
+++ src/contact/utils/adaptFormValues.spec.js
@@ -4,9 +4,20 @@ describe('`adaptFormValues`', () => {
   it('should return the right structure', () => {
     const actual = adaptFormValues({
       comments: 'yo',
+      dates: {
+        startDate: '📅',
+        endDate: '🗓',
+      },
+      property: '🏠',
       otherFormValue: 'sup',
     });
 
-    expect(actual).toEqual({ comment: 'yo', otherFormValue: 'sup' });
+    expect(actual).toEqual({
+      comment: 'yo',
+      endDate: '🗓',
+      otherFormValue: 'sup',
+      propertyId: '🏠',
+      startDate: '📅',
+    });
   });
 });
