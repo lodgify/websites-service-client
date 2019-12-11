ac16bfd39c87fa910d241384ac1bc866445bd77b
Jonny Gwillim <jonnygwillim@gmail.com>
2019-12-11 11:27:29 +0000

    feat(search): expose get search filters

---
+++ src/searchfilters/constants.js
@@ -0,0 +1,5 @@
+import { BASE_PATH_FRAGMENT, WEBSITE_ID_PLACEHOLDER } from '../constants';
+
+export const RESOURCE_NAME = 'searchfilters';
+
+export const PATHNAME_TEMPLATE = `${BASE_PATH_FRAGMENT}/search/${WEBSITE_ID_PLACEHOLDER}/filters`;
---
+++ src/searchfilters/index.js
@@ -0,0 +1,1 @@
+export { get as getSearchFilters } from './get';
